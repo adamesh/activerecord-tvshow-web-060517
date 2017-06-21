90a37ea9b6ae8fe6679c5ce61e66daabdf68013c
adamesh <adameshh@gmail.com>
2017-06-21 09:04:40 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,7 +7,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    self.where("rating = ?", self.highest_rating)
+    self.find_by(rating: self.highest_rating)
   end
 
   def self.lowest_rating
@@ -15,7 +15,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    self.where("rating = ?", self.lowest_rating)
+    self.find_by(rating: self.lowest_rating)
   end
 
   def self.ratings_sum
